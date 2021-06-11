f3fef4e83f9aa468b4c0c4206a04ca7f7c3ed6d2
robbyardha <robbyardha@gmail.com>
2021-06-11 16:51:19 +0700

    movtv repository not implement

--- app/src/main/java/com/ardhacodes/subs1_jetpack/data/MovTvDataSource.kt
+++ app/src/main/java/com/ardhacodes/subs1_jetpack/data/MovTvDataSource.kt
@@ -1,14 +1,26 @@
 package com.ardhacodes.subs1_jetpack.data
 
 import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
 import com.ardhacodes.subs1_jetpack.data.MovieTvEntity
+import com.ardhacodes.subs1_jetpack.data.source.datalocal.entity.MovieEntity
+import com.ardhacodes.subs1_jetpack.data.source.datalocal.entity.TvEntity
+import com.ardhacodes.subs1_jetpack.vo.Resource
 
 interface MovTvDataSource {
-    fun getPopularMovies(): LiveData<List<MovieTvEntity>>
+    fun getPopularMovies(sort: String): LiveData<Resource<PagedList<MovieEntity>>>
 
-    fun getMovieDetail(movieId: Int): LiveData<MovieTvEntity>
+    fun getMovieDetail(movieId: Int): LiveData<Resource<MovieEntity>>
 
-    fun getTv(): LiveData<List<MovieTvEntity>>
+    fun getFavMovie(): LiveData<PagedList<MovieEntity>>
 
-    fun getTvDetail(tvShowId: Int): LiveData<MovieTvEntity>
+    fun setFavMovie(movie: MovieEntity, state: Boolean)
+
+    fun getTv(sort: String): LiveData<Resource<PagedList<TvEntity>>>
+
+    fun getTvDetail(tvShowId: Int): LiveData<Resource<TvEntity>>
+
+    fun getFavTv(): LiveData<PagedList<TvEntity>>
+
+    fun setFavTv(tvShow: TvEntity, state: Boolean)
 }
--- app/src/main/java/com/ardhacodes/subs1_jetpack/data/MovTvRepository.kt
+++ app/src/main/java/com/ardhacodes/subs1_jetpack/data/MovTvRepository.kt
@@ -2,125 +2,30 @@ package com.ardhacodes.subs1_jetpack.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.paging.PagedList
+import com.ardhacodes.subs1_jetpack.data.source.datalocal.LocalDataSource
+import com.ardhacodes.subs1_jetpack.data.source.datalocal.entity.MovieEntity
+import com.ardhacodes.subs1_jetpack.data.source.datalocal.entity.TvEntity
 import com.ardhacodes.subs1_jetpack.data.source.remote.RemoteDataSource
-import com.ardhacodes.subs1_jetpack.data.source.remote.response.MovieResponse
+//import com.ardhacodes.subs1_jetpack.data.source.remote.response.MovieResponse
 import com.ardhacodes.subs1_jetpack.data.source.remote.response.TvResponse
+import com.ardhacodes.subs1_jetpack.utils.AppExecutors
+import com.ardhacodes.subs1_jetpack.vo.Resource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MovTvRepository private constructor(private val remoteDataSource: RemoteDataSource): MovTvDataSource{
-    companion object{
+class MovTvRepository private constructor(
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource,
+    private val appExecutors: AppExecutors): MovTvDataSource{
+    companion object {
         @Volatile
         private var instance: MovTvRepository? = null
-
-        fun getInstance(remoteDataSource: RemoteDataSource): MovTvRepository {
-            return instance ?: synchronized(this){
-                instance ?: MovTvRepository(remoteDataSource)
+        fun getInstance(remoteData: RemoteDataSource, localDataSource: LocalDataSource, appExecutors: AppExecutors): MovTvRepository =
+            instance ?: synchronized(this) {
+                instance ?: MovTvRepository(remoteData, localDataSource, appExecutors)
             }
-        }
-//        fun getInstance(remoteDataSource: RemoteDataSource): MovTvRepository =
-//            instance ?: synchronized(this){
-//                instance ?: MovTvRepository(remoteDataSource)
-//            }
-    }
-
-    override fun getPopularMovies(): LiveData<List<MovieTvEntity>> {
-        val listMovieResult = MutableLiveData<List<MovieTvEntity>>()
-        val CoroutineScp = CoroutineScope(Dispatchers.IO)
-        CoroutineScp.launch {
-            remoteDataSource.getPopularMovies(object : RemoteDataSource.LoadPopularMoviesCallback {
-                override fun responseAllMovie(movieResponse: List<MovieResponse>) {
-                    val movieList = ArrayList<MovieTvEntity>()
-                    for(response in movieResponse){
-                        val movie = MovieTvEntity(
-                            response.id,
-                            response.title,
-                            response.release_date,
-                            response.popularity,
-                            response.overview,
-                            response.vote_average,
-                            response.poster_path
-                        )
-                        movieList.add(movie)
-                    }
-                    listMovieResult.postValue(movieList)
-                }
-
-            })
-        }
-
-        return listMovieResult
-    }
-
-    override fun getMovieDetail(movieId: Int): LiveData<MovieTvEntity> {
-        val movResult = MutableLiveData<MovieTvEntity>()
-        val CoroutineScp = CoroutineScope(Dispatchers.IO)
-        CoroutineScp.launch {
-            remoteDataSource.getMovieDetail(movieId, object: RemoteDataSource.LoadMovieDetailCallback{
-                override fun responseDetailMovie(movieResponse: MovieResponse) {
-                    val movie = MovieTvEntity(
-                        movieResponse.id,
-                        movieResponse.title,
-                        movieResponse.release_date,
-                        movieResponse.popularity,
-                        movieResponse.overview,
-                        movieResponse.vote_average,
-                        movieResponse.poster_path
-                    )
-                    movResult.postValue(movie)
-                }
-            })
-        }
-        return movResult
-    }
-
-    override fun getTv(): LiveData<List<MovieTvEntity>> {
-        val listTvShowResult = MutableLiveData<List<MovieTvEntity>>()
-        val CoroutineScp = CoroutineScope(Dispatchers.IO)
-        CoroutineScp.launch {
-            remoteDataSource.getTvList(object : RemoteDataSource.LoadTvCallback{
-                override fun responseAllTv(tvShowResponse: List<TvResponse>) {
-                    val tvShowList = ArrayList<MovieTvEntity>()
-                    for(response in tvShowResponse){
-                        val tvShow = MovieTvEntity(
-                            response.id,
-                            response.title,
-                            response.release_date,
-                            response.popularity,
-                            response.overview,
-                            response.vote_average,
-                            response.poster_path
-                        )
-                        tvShowList.add(tvShow)
-                    }
-                    listTvShowResult.postValue(tvShowList)
-                }
-            })
-        }
-        return listTvShowResult
-    }
-
-    override fun getTvDetail(tvShowId: Int): LiveData<MovieTvEntity> {
-        val tvShowResult = MutableLiveData<MovieTvEntity>()
-        val CoroutineScp = CoroutineScope(Dispatchers.IO)
-        CoroutineScp.launch {
-            remoteDataSource.getTvDetail(tvShowId, object :  RemoteDataSource.LoadTvDetailCallback {
-                override fun responseDetailTv(tvShowResponse: TvResponse) {
-                    val tvShow = MovieTvEntity(
-                        tvShowResponse.id,
-                        tvShowResponse.title,
-                        tvShowResponse.release_date,
-                        tvShowResponse.popularity,
-                        tvShowResponse.overview,
-                        tvShowResponse.vote_average,
-                        tvShowResponse.poster_path
-                    )
-                    tvShowResult.postValue(tvShow)
-                }
-            })
-        }
-        return tvShowResult
     }
 
 }
